b886558690da91ae878f09a8d1784a44fb5b5c72
hanayashiki <cwang592@usc.edu>
2022-01-10 23:50:16 +0800

    feat: add theme-color

--- scripts/createTemplate.ts
+++ scripts/createTemplate.ts
@@ -15,9 +15,10 @@ export const createTemplate = (
 
   <link rel="stylesheet" href="${cssUrl}">
 
-  <meta name="msapplication-TileColor" content="#000000">
-  <meta name="theme-color" content="#000000">
+  <meta name="msapplication-TileColor" content="#111827">
+  <meta name="theme-color" content="#111827">
   <meta name="description" content="专注于 JavaScript 的技术博客。A tech-blog mainly focused on JavaScript. ">
+  <meta name="apple-mobile-web-app-capable" content="yes">
   <script async defer data-domain="blog.chenyu.pw" src="https://plausible.monoid.co.jp/js/plausible.js"></script>
   <script>window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>
 </head>
