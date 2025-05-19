32ae775068c51bc8a123d3c542c5e06ee53c3efd
hanayashiki <wangchenyu2017@gmail.com>
2025-05-20 00:50:42 +0900

    Updated shareBlog function, removed Plausible icon, and added new blog post placeholder

--- src/libs/share.ts
+++ src/libs/share.ts
@@ -3,7 +3,8 @@ import { Blog } from "@models/Blog";
 import copy from 'copy-to-clipboard';
 
 export const shareBlog = (blog: Blog) => {
+  const currentDomain = window.location.origin;
   plausible('share');
-  copy(`${blog.data.title} https://blog.chenyu.pw/blogs/${blog.data.slug}`);
+  copy(`${blog.data.title} ${currentDomain}/blogs/${blog.data.slug}`);
   alert('链接已经复制!');
 };
